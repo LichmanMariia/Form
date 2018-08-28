688461e69f497305385fd7d4b7e2d2969dbaa74b
Lenovo <Lenovo@10.4.1.66>
2018-08-28 19:58:15 +0300

    12

--- src/main/java/org/itstep/StartServlet.java
+++ src/main/java/org/itstep/StartServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/")
-public class MainServlet extends HttpServlet {
+public class StartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static String pages = "/WEB-INF/pages/myPages.html";
 
